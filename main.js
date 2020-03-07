acc302e49e9846b402ba92cf02e3dff85b0ef391
Raymond Ward <rw468@kent.ac.uk>
2020-03-07 01:19:18 +0000

    Added GET request function + other minor functionality
    Created the method getArticleJSON which when given the article number will retrieve article directy from github using an XML http request. WIll later call a callback function but for now just outputs paragraphs and images onto site

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
 
 
-$.get(
-    "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-1.json",
-    function(data) {
-       console.log(data);
-    }
-);
+
+
+function getArticleJSON (articleNum)
+{
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.responseType = "json";
+
+    //The github url to the raw json files is supplied and concatenated with the given article number.
+    httpRequest.open("GET", "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-" + articleNum + ".json");
+
+    //Called when the state of the XMLHttpRequest changes state 
+    httpRequest.onreadystatechange = () =>
+    {
+        //Operation is done && status == done (200)
+        if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status == 200)
+        {
+            articleJSON = httpRequest.response;
+            console.log(articleJSON);
+            
+
+            //Loops through body of article and outputs paragraphs and images
+            //Place holder for callback function later
+            for (let i = 0; i < articleJSON.body.length; i++)
+            {   
+                if (articleJSON.body[i].type === "paragraph")
+                {
+                    document.getElementById("articleParagraphs").innerHTML += articleJSON.body[i].model.text;
+                }
+                else if (articleJSON.body[i].type === "image")
+                {
+                    //console.log(articleJSON)
+                    document.getElementById("photos").innerHTML += "<img src = " + articleJSON.body[i].model.url + ">"
+                }
+
+            }
+        }
+    };
+
+
+    
+    httpRequest.send();
+
+}
+
+
 
 
