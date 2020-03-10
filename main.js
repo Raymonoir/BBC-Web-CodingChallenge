1d8ff89d9cf68f3284f72cdcdbd9539c39557fa2
Raymond Ward <rw468@kent.ac.uk>
2020-03-10 00:40:36 +0000

    Working on changing articles
    The functionality to change articles has now been implemented, I decided to revise my getArticleJSON method to return a promise, which makes it easier to handle the asynchronous http request. I have also added a timestamp to the end of the picsum link so each image slide is a new image instead of the same one. Finally I added the list section to the article as well as the correct style for titles and headings

--- main.js
+++ main.js
@@ -4,113 +4,66 @@
 //window.addEventListener("offline", functiom (){})
 
 
-// let myOwnArticleImageArray = ['https://i.picsum.photos/id/502/640/420.jpg', 'https://i.picsum.photos/id/982/640/420.jpg', 'https://i.picsum.photos/id/358/640/420.jpg'];
 let currentArticleImageArray = [];
-let currentArticleSections = [];
-
-
-let preLoadedArticles = [getArticleJSON(1)];
-
-
+var currentArticleSections = [];
 let slideIndex = 0;
-let currentArticlePos = 1;
+var currentArticlePos = 1;
 
 
 
-function preLoadSurroundingArticlesJSON(currentArticlePos)
-{
-    if (currentArticlePos === 1)
-    {
-        preLoadedArticles.push(getArticleJSON(2));
-    }
-    else if (currentArticlePos === 5)
-    {
-        preLoadedArticles.push(getArticleJSON(4));
-    }
-    else
-    {
-        preLoadedArticles.push(getArticleJSON(currentArticlePos-1));
-        preLoadedArticles.push(getArticleJSON(currentArticlePos+1));
-    }
-}
-
-
-
-function clearCurrentArticleHTML()
-{
-    //document.getElementById("articleText").innerHTML = "";
-    //document.getElementById("articleImageID").src = "";
-
-    currentArticleImageArray = [];
-    currentArticleParagraphArray = [];
-
-    slideIndex = 0;
-    currentArticlePos = 1;
-    preLoadedArticles = [];
-
-
-}
-
-//Section Types: Heading, Paragraph, Image (DONE), List 
 //Function to organise article sections and lay them out on the page
 function setArticleHTML()
 {
-    console.log("here")
+    //Sets the article title which is first in the array and then pop it from the front
+    document.getElementById("articleParagraphs").innerHTML += "<h2> <u>" + currentArticleSections[0] + "</u></h2><br>";
+    currentArticleSections.shift();
+
+    //Loop through all items in the article sections array
     for (let i = 0; i < currentArticleSections.length; i++)
     {
-        if (currentArticleSections[i].type = "heading")
-        {
-            document.getElementById("articleParagraphs").innerHTML += "<h3>" + currentArticleSections[i].model.text + "</h3>";
-        }
-        else if (currentArticleSections[i].type = "paragraph")
-        {
+        //Sort each article section by its type and add it to the required section
 
-            document.getElementById("articleParagraphs").innerHTML += currentArticleSections[i].model.text;
-        }
-        else if (currentArticleSections[i].type = "list")
+        if (currentArticleSections[i].type === "heading")
         {
+            document.getElementById("articleParagraphs").innerHTML += "<h3>" + currentArticleSections[i].model.text + "</h3><br>";
+        }
+        else if (currentArticleSections[i].type === "paragraph")
+        {
+            document.getElementById("articleParagraphs").innerHTML += currentArticleSections[i].model.text + " <br><br>";
+        }
+        else if (currentArticleSections[i].type === "list")
+        {
+            
 
+            //Add any lists by looping through all items and then add prepared string to document
+            var articleList = "<ul>";
+
+            for (j = 0; j < currentArticleSections[i].model.items.length; j++ )
+            {
+                articleList += "<li>" + currentArticleSections[i].model.items[j] +  "</li>";
+            }
+
+            articleList += "</ul>";
+
+            document.getElementById("articleParagraphs").innerHTML += articleList;
         }
     }
 
 }
 
-
-function changeArticleJSON (articlePosChange)
-{
-    //Change article if the new article if one of the 5
-    if (currentArticlePos + articlePosChange > 0 && currentArticlePos + articlePosChange < 6)
-    {
-        currentArticlePos += articlePosChange;
-        
-        if (articlePosChange === -1)
-        {
-            //Get preloaded article which is previous to current article
-            setArticleHTML(preLoadedArticles[0]);
-
-        }
-        else
-        {
-            //Get the preloaded article after current article
-            setArticleHTML(preLoadedArticles[1]);
-        }
-
-    }
-
-    preLoadSurroundingArticlesJSON(currentArticlePos)
-
-}
 
 //Function to sort through contents of article and display them in their alloted sections
 function organiseArticleContents (articleJSON)
 {
            //Loops through length of article body
+           currentArticleSections.push(articleJSON.title);
+
             for (let i = 0; i < articleJSON.body.length; i++)
-            {   
+            { 
                 //If its an image, add the url to the list of site images.
                 if (articleJSON.body[i].type === "image")
                 {
-                    currentArticleImageArray.push(articleJSON.body[i].model.url);
+                    currentArticleImageArray.push(articleJSON.body[i].model.url + "?" + new Date().getTime());
                 }
                 //If the section type is anything but an image, add to article section array
                 else
@@ -122,6 +75,8 @@ function organiseArticleContents (articleJSON)
 
             if (currentArticleImageArray.length != 0)
             {
+                document.getElementById("articleSlides").style.display = "block";
+
                 //Set the current image to first image in  array
                 document.getElementById("articleImageID").src = currentArticleImageArray[0];
 
@@ -132,9 +87,11 @@ function organiseArticleContents (articleJSON)
             //If there are no images on the page, remove the div of the article slides
             else
             {
-                document.getElementById("articleSlides").remove();
+                document.getElementById("articleSlides").style.display = "none";
             }
-        }
+
+        
+}
 
 
         
@@ -145,34 +102,108 @@ function changeArticleImage(slidePosChange)
     //As long as the new position is in range of the image array, change the image src.
     if (slideIndex + slidePosChange > -1 && slideIndex + slidePosChange < currentArticleImageArray.length )
     {
-        document.getElementById("articleImageID").src = currentArticleImageArray[slideIndex + slidePosChange];
+        document.getElementById("articleImageID").src = currentArticleImageArray[slideIndex + slidePosChange] + "?" + new Date().getTime();
         slideIndex += slidePosChange; 
         document.getElementById("articleImageNumberID").innerHTML = (slideIndex + 1) + " / " + currentArticleImageArray.length;
     }
           
 }
 
-//Function to get a single article in .JSON format
+//Function to go to the next article
+function getNextArticle ()
+{
+    //Only move if new article position is in 1-5
+    if(currentArticlePos + 1 < 6)
+    {
+        clearArticleHTML();
+
+        getArticleJSON(currentArticlePos+1).then(function()
+            {
+                setArticleHTML();
+            }, 
+            function(error)
+            {
+                console.log(error);
+    
+            });
+
+        currentArticlePos +=1;
+
+    }
+
+   
+
+}
+
+//Function to go to previous article
+function getPrevArticle()
+{
+    //Only move if new article position is in 1-5
+    if(currentArticlePos-1 > 0)
+    {
+        clearArticleHTML();
+
+        getArticleJSON(currentArticlePos-1).then(function()
+        {
+            setArticleHTML();
+        }, 
+        function(error)
+        {
+            console.log(error);
+
+        });
+
+        currentArticlePos -=1;
+    }
+
+
+
+}
+
+//Function to reset and clear all variables which change per article
+function clearArticleHTML()
+{
+    currentArticleImageArray = [];
+    currentArticleSections = [];
+    slideIndex = 0;
+    document.getElementById("articleParagraphs").innerHTML = "";
+
+
+}
+
+//Function to get a single article in .JSON format which is returned in a promise
 function getArticleJSON (articleNum)
 {
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.responseType = "json";
-
-    //The github url to the raw json files is supplied and concatenated with the given article number.
-    httpRequest.open("GET", "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-" + articleNum + ".json");
-
-    //Called when the state of the XMLHttpRequest changes state 
-    httpRequest.onreadystatechange = () =>
+    return new Promise(function(resolve, reject)
     {
-        //Operation is done && status == done (200)
-        if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status == 200)
-        {
-            articleJSON = httpRequest.response;
-            organiseArticleContents(articleJSON);
-        }
-    };
 
-    httpRequest.send();
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.responseType = "json";
+
+        //The github url to the raw json files is supplied and concatenated with the given article number.
+        httpRequest.open("GET", "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-" + articleNum + ".json");
+
+        //Called when the state of the XMLHttpRequest changes state 
+        let articleJSON;
+
+        httpRequest.onloadend = () =>
+        {
+            //Operation is done && status == done (200)
+            if (httpRequest.status === 200 && httpRequest.readyState === httpRequest.DONE)
+            {
+                articleJSON = httpRequest.response;
+                organiseArticleContents(articleJSON);
+                resolve();
+            }
+            else
+            {
+                reject(Error(httpRequest.statusText));
+            }
+        }
+
+        httpRequest.send();
+
+    });  
 
 }
 
